c68f01d7da357e4157b6b27c941238faaa35454c
alexio42 <116265425+alexio42@users.noreply.github.com>
2024-07-25 11:10:27 +0100

    Add first challenges

---
+++ basic/challenges/01-vars.js
@@ -0,0 +1,11 @@
+let firstName, last_name;
+firstName = 'John';
+last_name = 'Peters';
+console.log(firstName, last_name);
+
+
+let address = "main street";
+console.log(address);
+
+address = "first street";
+console.log(address);
