eb23616f7884ec1c76bf1881889f7f370832445c
alexio42 <116265425+alexio42@users.noreply.github.com>
2024-08-22 12:02:02 +0100

    Add filter array method example

---
+++ basic/arrayMethods/find.js
@@ -0,0 +1,33 @@
+// find
+// returns single instance - (in this case object)
+// returns first mathc, if no match, returns undefined
+// great for getting unique value
+
+const people = [
+  { name: 'bob', age: 20, position: 'developer', id: 1 },
+  { name: 'peter', age: 25, position: 'designer', id: 2 },
+  { name: 'susy', age: 30, position: 'the boss', id: 3 },
+  { name: 'anna', age: 35, position: 'the boss', id: 4 },
+];
+
+const names = ['bob', 'peter', 'susy'];
+
+console.log(
+  names.find(function (name) {
+    return name === 'bob';
+  })
+);
+
+// returns object
+const person = people.find(function (person) {
+  return person.id === 3;
+});
+
+console.log(person); // can access name using person.name
+
+//returns array
+const person2 = people.filter(function (person) {
+  return person.id === 3;
+});
+
+console.log(person2); // need to use person2[0].name to access name
