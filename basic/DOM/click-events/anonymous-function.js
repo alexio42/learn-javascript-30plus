8d035c7e62e7d861a929bc870bc628e6b4040f61
alexio42 <116265425+alexio42@users.noreply.github.com>
2024-09-05 09:57:24 +0100

    Add callback function example

--- basic/DOM/click-events/anonymous-function.js
+++ basic/DOM/click-events/anonymous-function.js
@@ -6,5 +6,5 @@ const btn = document.querySelector('.btn');
 const heading = document.querySelector('h2');
 
 btn.addEventListener('click', function () {
-  heading.classList.toggle('red');
+  heading.classList.add('red');
 });
